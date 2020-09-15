74c8b4a8b18a116ae15ebc585fb199151621a89c
Sommier <marie.a.s45@gmail.com>
2020-09-15 16:56:51 +0200

    ex1 finished

---
+++ PHP_niveau2/PHP-Exo1-Conjecture-Syracuse/index.php
@@ -0,0 +1,26 @@
+<?php
+function changechiffre($chiffrentier){
+    $chiffrentier = intval($chiffrentier);
+    if ($chiffrentier<=0) {
+    $chiffrentier = 1;
+    }
+return $chiffrentier;
+}
+
+function syracuse($newchiffre){
+echo $newchiffre;
+    do {
+    if ($newchiffre%2===1){
+    $newchiffre = ($newchiffre*3)+1;
+    echo " ".($newchiffre);
+    }
+    else {
+    $newchiffre = $newchiffre/2;
+    echo " ".($newchiffre);
+    }
+}
+while ($newchiffre != 1);
+
+return $newchiffre;
+}
+syracuse(changechiffre(877.67));
