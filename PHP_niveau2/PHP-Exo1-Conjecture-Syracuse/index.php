35a27e31f709f1747513136cc796250dbbb6a8b4
Sommier <marie.a.s45@gmail.com>
2020-09-21 11:20:35 +0200

    formated php

--- PHP_niveau2/PHP-Exo1-Conjecture-Syracuse/index.php
+++ PHP_niveau2/PHP-Exo1-Conjecture-Syracuse/index.php
@@ -1,26 +1,26 @@
 <?php
-function changechiffre($chiffrentier){
+function changechiffre($chiffrentier)
+{
     $chiffrentier = intval($chiffrentier);
-    if ($chiffrentier<=0) {
-    $chiffrentier = 1;
+    if ($chiffrentier <= 0) {
+        $chiffrentier = 1;
     }
-return $chiffrentier;
+    return $chiffrentier;
 }
 
-function syracuse($newchiffre){
-echo $newchiffre;
+function syracuse($newchiffre)
+{
+    echo $newchiffre;
     do {
-    if ($newchiffre%2===1){
-    $newchiffre = ($newchiffre*3)+1;
-    echo " ".($newchiffre);
-    }
-    else {
-    $newchiffre = $newchiffre/2;
-    echo " ".($newchiffre);
-    }
-}
-while ($newchiffre != 1);
+        if ($newchiffre % 2 === 1) {
+            $newchiffre = ($newchiffre * 3) + 1;
+            echo " " . ($newchiffre);
+        } else {
+            $newchiffre = $newchiffre / 2;
+            echo " " . ($newchiffre);
+        }
+    } while ($newchiffre != 1);
 
-return $newchiffre;
+    return $newchiffre;
 }
 syracuse(changechiffre(877.67));
