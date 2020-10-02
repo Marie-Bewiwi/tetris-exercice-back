e40c1e0077fe21790ee790785f6652b000506e60
Sommier <marie.a.s45@gmail.com>
2020-10-02 12:11:07 +0200

    succeed to get rss in html

--- PHP_niveau4/boss4_agregfluxrss/getrss.php
+++ PHP_niveau4/boss4_agregfluxrss/getrss.php
@@ -37,19 +37,6 @@ function loadrss($url)
         echo $item->getElementsbyTagName('pubDate')->item(0)->childNodes->item(0)->nodeValue;
     }
 
-    /*echo ("<p>Titre de la news 1 : ". $x->item(0)->getElementsByTagName('title')->item(0)->childNodes->item(0)->nodeValue . "</p>");
-    echo ("<p>Titre de la news 2 : ". $x->item(1)->getElementsByTagName('title')->item(0)->childNodes->item(0)->nodeValue . "</p>");
-    echo ("<p>Titre de la news 3 : ". $x->item(2)->getElementsByTagName('title')->item(0)->childNodes->item(0)->nodeValue . "</p>");*/
-
-    /* vous devrez faire une boucle pour itérer sur tous les éléments
- * à chaque élément vous pourrez récupérer les infos suivantes :
- * - titre de la news
- * - lien vers la news complète
- * - image associée à la news
- * - texte de la news
- * - date de la news
- *
- * C'est à partie de toutes ces infos que vous générerez le code HTML */
 }
 
 // on appelle la fonction qu'on a écrite sur le flux RSS de notre choix
